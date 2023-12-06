a0974f9c331ee832c26dc1baa5fda26112f52b21
presley-wang <153104436+presley-wang@users.noreply.github.com>
2023-12-06 08:59:25 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,113 +3,114 @@ namespace SpriteKind {
     export const Goal = SpriteKind.create()
     export const Obstacle = SpriteKind.create()
 }
-
-controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     myDart.stopDart()
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     myDart.throwDart()
 })
-sprites.onOverlap(SpriteKind.Football, SpriteKind.Obstacle, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+sprites.onOverlap(SpriteKind.Football, SpriteKind.Obstacle, function (sprite, otherSprite) {
     game.splash("It was an interception")
     game.gameOver(false)
 })
-sprites.onOverlap(SpriteKind.Football, SpriteKind.Goal, function on_on_overlap2(sprite2: Sprite, otherSprite2: Sprite) {
+sprites.onOverlap(SpriteKind.Football, SpriteKind.Goal, function (sprite, otherSprite) {
     game.splash("Goal!!")
     game.gameOver(true)
 })
-let mySprite2 : Sprite = null
-let myDart : Dart = null
-let question = game.askForString("")
-myDart = darts.create(img`
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+let myDart: Dart = null
+let likes = game.askForString("do you like football?")
+if (likes == "yes") {
+    myDart = darts.create(img`
         . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . e e e e e e e . . . . . . 
-            . . e 1 e 1 e 1 e 1 e . . . . . 
-            . . . e e e e e e e . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . .
-    `, SpriteKind.Football)
-myDart.setTrace()
-myDart.controlWithArrowKeys()
-let mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . e e e e e e e . . . . . . 
+        . . e 1 e 1 e 1 e 1 e . . . . . 
+        . . . e e e e e e e . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Football)
+    myDart.setTrace()
+    myDart.controlWithArrowKeys()
+    mySprite = sprites.create(img`
         .............cc.
-            ............cbbc
-            ............cbbc
-            ...........bdcc.
-            ...........bdbb.
-            ..........bddc..
-            ..........bdbb..
-            .........bddc...
-            .........bdbb...
-            ........bddc....
-            ........bdbb....
-            .......bddc.....
-            .......bdbb.....
-            ......bddc......
-            ......bdbb......
-            .....bddc.......
-            .....bdbb.......
-            ....bddc........
-            ....bdbb........
-            ...bddc.........
-            ...bdbb.........
-            ..bddc..........
-            ..bdbb..........
-            .bddc...........
-            .bdbb...........
-            b1dc............
-            b11c............
-            b11c............
-            b11c.........cc.
-            b11c........cbbc
-            b11c........cbbc
-            b11c.......bdcc.
-            b11c.......bdbb.
-            b11c......bddc..
-            b11bccc...bdbb..
-            b11bbbbcccddc...
-            b11bcccbccdbb...
-            b11b..ccbddc....
-            b11b...ccdbbc...
-            b11b...bddcbc...
-            b11b...bdbbcbc..
-            b11b..bddc.fbc..
-            b11b..bdbb.fbf..
-            b11b.bddc..fcf..
-            b11b.bdbb..fcf..
-            b11bbddc...fcf..
-            b11bbdbb...fcf..
-            b11bddc...cfcfc.
-            b11ddbb..cbfcfbc
-            b1dddc...cdfffdc
-            b1ddbb...cdcfcdc
-            cdddc....cbdddbc
-            cddbb....cbbbbbc
-            cddc.....cbbbbbc
-            cdbb.....cbbbbbc
-            .cc......cbbbbbc
-            .........cbbbbbc
-            .........cbbbbbc
-            .........cbbbbbc
-            .........8bbbbb8
-            .........8bbbbb8
-            .........6bbbbb6
-            ..........6bbb6.
-            ...........666..
-    `, SpriteKind.Goal)
-mySprite.setPosition(150, 95)
-scene.setBackgroundImage(img`
-    9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999995555555555555555555555555555
+        ............cbbc
+        ............cbbc
+        ...........bdcc.
+        ...........bdbb.
+        ..........bddc..
+        ..........bdbb..
+        .........bddc...
+        .........bdbb...
+        ........bddc....
+        ........bdbb....
+        .......bddc.....
+        .......bdbb.....
+        ......bddc......
+        ......bdbb......
+        .....bddc.......
+        .....bdbb.......
+        ....bddc........
+        ....bdbb........
+        ...bddc.........
+        ...bdbb.........
+        ..bddc..........
+        ..bdbb..........
+        .bddc...........
+        .bdbb...........
+        b1dc............
+        b11c............
+        b11c............
+        b11c.........cc.
+        b11c........cbbc
+        b11c........cbbc
+        b11c.......bdcc.
+        b11c.......bdbb.
+        b11c......bddc..
+        b11bccc...bdbb..
+        b11bbbbcccddc...
+        b11bcccbccdbb...
+        b11b..ccbddc....
+        b11b...ccdbbc...
+        b11b...bddcbc...
+        b11b...bdbbcbc..
+        b11b..bddc.fbc..
+        b11b..bdbb.fbf..
+        b11b.bddc..fcf..
+        b11b.bdbb..fcf..
+        b11bbddc...fcf..
+        b11bbdbb...fcf..
+        b11bddc...cfcfc.
+        b11ddbb..cbfcfbc
+        b1dddc...cdfffdc
+        b1ddbb...cdcfcdc
+        cdddc....cbdddbc
+        cddbb....cbbbbbc
+        cddc.....cbbbbbc
+        cdbb.....cbbbbbc
+        .cc......cbbbbbc
+        .........cbbbbbc
+        .........cbbbbbc
+        .........cbbbbbc
+        .........8bbbbb8
+        .........8bbbbb8
+        .........6bbbbb6
+        ..........6bbb6.
+        ...........666..
+        `, SpriteKind.Goal)
+    mySprite.setPosition(150, 95)
+    scene.setBackgroundImage(img`
+        9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999995555555555555555555555555555
         9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999995555555555555555555555555555
         9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999995555555555555555555555555555
         9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999995555555555555555555555555555
@@ -229,25 +230,28 @@ scene.setBackgroundImage(img`
         7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
         7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
         7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
-`)
-for (let index = 0; index < 3; index++) {
-    mySprite2 = sprites.create(img`
+        `)
+    for (let index = 0; index < 3; index++) {
+        mySprite2 = sprites.create(img`
             . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . 2 2 2 2 . . . . . . . 
-                    . . . . . 2 2 2 2 . . . . . . . 
-                    . . . . . d d 2 2 . . . . . . . 
-                    . . . . 1 1 1 2 2 . . . . . . . 
-                    . . . . 5 5 5 5 5 5 5 . . . . . 
-                    . d 2 2 5 2 2 2 2 2 5 . . . . . 
-                    . d 2 2 2 2 5 5 5 2 . . . . . . 
-                    . . . . . 2 2 2 5 2 . . . . . . 
-                    . . . . 2 2 2 2 5 2 2 . . . . . 
-                    . . . . 2 2 2 2 2 2 2 . . . . . 
-                    . . . . 5 5 5 5 5 5 5 . . . . . 
-                    . . . . . . 2 . . 2 . . . . . . 
-                    . . . . . 2 2 . 2 2 . . . . . . 
-                    . . . . . f f . f f . . . . . .
-        `, SpriteKind.Obstacle)
-    mySprite2.setPosition(randint(0, 110), randint(20, 90))
+            . . . . . . . . . . . . . . . . 
+            . . . . . 2 2 2 2 . . . . . . . 
+            . . . . . 2 2 2 2 . . . . . . . 
+            . . . . . d d 2 2 . . . . . . . 
+            . . . . 1 1 1 2 2 . . . . . . . 
+            . . . . 5 5 5 5 5 5 5 . . . . . 
+            . d 2 2 5 2 2 2 2 2 5 . . . . . 
+            . d 2 2 2 2 5 5 5 2 . . . . . . 
+            . . . . . 2 2 2 5 2 . . . . . . 
+            . . . . 2 2 2 2 5 2 2 . . . . . 
+            . . . . 2 2 2 2 2 2 2 . . . . . 
+            . . . . 5 5 5 5 5 5 5 . . . . . 
+            . . . . . . 2 . . 2 . . . . . . 
+            . . . . . 2 2 . 2 2 . . . . . . 
+            . . . . . f f . f f . . . . . . 
+            `, SpriteKind.Obstacle)
+        mySprite2.setPosition(randint(0, 110), randint(20, 90))
+    }
+} else {
+    game.gameOver(false)
 }
